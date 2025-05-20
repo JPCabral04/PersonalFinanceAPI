9125c2e833f9d593ae36dbb862bd87726675978c
JPCabral04 <jpqcabral04@gmail.com>
2025-05-20 08:58:18 -0300

    feat(user): Add bcrypt and jsonwebtoken

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 
 @Entity()
 export class User {
@@ -13,4 +13,7 @@ export class User {
 
   @Column()
   password: string;
+
+  // @OneToMany(() => Account, account => account.user)
+  // accounts: Account[];
 }
