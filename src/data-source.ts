6555524d633c0e595fe34931d52e3e74125bd1ef
JPCabral04 <jpqcabral04@gmail.com>
2025-05-20 19:38:23 -0300

    feat(data-source): include Account and Transaction entities in AppDataSource

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm';
 import { User } from './entity/User';
-// import { Account } from './entity/Account';
-// import { Transaction } from './entity/Transaction';
+import { Account } from './entity/Account';
+import { Transaction } from './entity/Transaction';
 import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
@@ -9,5 +9,5 @@ export const AppDataSource = new DataSource({
   url: process.env.DATABASE_URL,
   synchronize: true,
   logging: true,
-  entities: [User],
+  entities: [User, Account, Transaction],
 });
