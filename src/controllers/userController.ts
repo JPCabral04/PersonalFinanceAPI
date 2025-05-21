72bf2ff26bbd92415a2c209db4f0570c779a8739
JPCabral04 <jpqcabral04@gmail.com>
2025-05-20 21:09:00 -0300

    feat(user-controller): implement user management functions for CRUD operations

---
+++ src/controllers/userController.ts
@@ -0,0 +1,65 @@
+import status from 'http-status';
+import { AppDataSource } from '../data-source';
+import { User } from '../entity/User';
+
+const userRepo = AppDataSource.getRepository(User);
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await userRepo.find();
+    res.json(users);
+  } catch (err) {
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Erro ao buscar usuários' });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await AppDataSource.getRepository(User).findOneBy({
+      id: parseInt(req.params.id),
+    });
+    if (!user) return res.status(404).json({ error: 'Usuário não encontrado' });
+    res.json(user);
+  } catch (err) {
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Erro ao buscar usuários' });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const user = await AppDataSource.getRepository(User).findOneBy({
+      id: parseInt(req.params.id),
+    });
+    if (!user) return res.status(404).json({ error: 'Usuário não encontrado' });
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+    await userRepo.save(user);
+
+    res.json(user);
+  } catch (err) {
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Erro ao atualizar usuário' });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const result = await userRepo.delete(id);
+    if (result.affected == 0)
+      return res.status(404).json({ error: 'Usuário não encontrado' });
+
+    res.json(status.ACCEPTED);
+  } catch (err) {
+    res
+      .status(status.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Erro ao deletar usuário' });
+  }
+};
